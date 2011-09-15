9f601f0f7f2746f6bb674f2751880741130a2a04
Lars Moelleken <voku@FUCKUP.(none)>
2011-09-16 01:32:07 +0200

    + update

--- src/com/android/phone/Ringer.java
+++ src/com/android/phone/Ringer.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2006 The Android Open Source Project
+ * Copyright (C) 2011 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -324,7 +325,6 @@ public class Ringer {
             };
         }
     }
-
     private static void log(String msg) {
         Log.d(LOG_TAG, msg);
     }
